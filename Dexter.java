bc1fae88ad037cbfde7cc479c6d1b91934b69198
cokafor1 <cokafor1@slu.edu>
2017-01-31 10:37:30 -0500

    Add files via upload

--- Dexter.java
+++ Dexter.java
@@ -9,13 +9,13 @@ public class Dexter {
 	
 	public Dexter(int x, int y){
 		
-		this.x = x;
-		this.y = y;
+		this.x = 12*x;
+		this.y = 12*y;
 		
 		ImageIcon image = new ImageIcon("C:\\Users\\Udo\\workspace\\Final Project 2\\src\\Dexter_Icon.jpg");
 		Dexter = image.getImage();
 		
-		x = 0;
+		x = 12;
 		y = 0;
 	}
 
@@ -41,14 +41,23 @@ public class Dexter {
 		int tempX = (this.x + x)/12;
 		int tempY = (this.y + y)/12;
 		
-		if (Maze[tempX][tempY] != 0) { //check if move is valid
-			this.x += tempX;
-			this.y += tempY; // put dexter at x,y update location if valid
+		if (Maze[tempX][tempY] == 0) { //check if move is valid
+			this.x += tempX/12;
+			this.y += tempY/12; // put dexter at x,y update location if valid
+		}
+		else if (Maze[tempY][tempX] == 1){
+			this.x = x/12;
+			this.y = y/12;
+		}
+		else{
+			this.x += tempX/12;
+			this.y += tempY/12;
+		}
 		}
 	
 	}
 	
-}
+
 
 /*
  * Control class (add to panel2)
